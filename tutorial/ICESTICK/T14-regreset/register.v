b0c2ef41e31f8ed375ca848bca51e903a76863e4
Nicolas Sanchez Flores <n.sanchez.f10@gmail.com>
2023-12-12 20:28:32 +0000

    unfinished array of registers

--- tutorial/ICESTICK/T14-regreset/register.v
+++ tutorial/ICESTICK/T14-regreset/register.v
@@ -1,32 +1,16 @@
-//--------------------------------------------------------------------------
-//-- register.v
-//-- Registro genérico de N bits, con reset asíncrono activo a nival bajo
-//--------------------------------------------------------------------------
-//-- (C) BQ. August 2015. Written by Juan Gonzalez (Obijuan)
-//--------------------------------------------------------------------------
-//-- Parámetros:  N: Numero de bits
-//--             INI: Valor inicial del registro al hacer el reset
-//--------------------------------------------------------------------------
+module register #(parameter WIDTH = 4, INI = 4'b0000)
+(
+    input clk,
+    input rst_bar,
+    input din [WIDTH-1:0],
+    output reg dout [WIDTH-1:0]
+);
 
-module register (rst, clk, din, dout);
-
-//-- Parametros: número de bits del registro
-parameter N = 4;     //-- Número de bits del registro
-parameter INI = 0;   //-- Valor inicial
-
-//-- Declaración de los puertos
-input wire rst;
-input wire clk;
-input wire [N-1:0] din;
-output reg [N-1:0] dout;
-
-//-- Registro
-always @(posedge(clk))
-  if (rst == 0)
-    dout <= INI; //-- Inicializacion
-  else
-    dout <= din; //-- Funcionamiento normal
+always @ (posedge(clk)) begin
+    if (rst_bar == 0)
+        dout <= INI;
+    else
+        dout <= din;
+    end
 
 endmodule
-
-
--- tutorial/ICESTICK/T14-regreset/regreset.v
+++ tutorial/ICESTICK/T14-regreset/regreset.v
@@ -1,60 +1,31 @@
-//-----------------------------------------------------------------------------
-//-- Secuenciador de 2 estados a partir de dos registros de 4 bits con
-//-- inicialización de carga
-//-- (C) BQ. August 2015. Written by Juan Gonzalez (obijuan)
-//-----------------------------------------------------------------------------
-//-- License GPL
-//-----------------------------------------------------------------------------
+module regreset #(parameter NP = 2)
+(
+    input clk,
+    output data [3:0]
+);
+integer INI = 0;
+wire [1:0]outputs[3:0];
 
-module regreset(input wire clk, output wire [3:0] data);
+genvar i;
+for (i = 0; i < 2; i=i+1) begin:reg_array 
+    INI = INI + 1;
+    register #(.WIDTH(4), .INI(INI))
+        dut (
+            .clk(clk_pres),
+            .rst_bar(inisig),
+            .din(outputs()),
+            .dout(output(i+1))
+        );
+end
 
-//-- Parametros del secuenciador:
-parameter NP = 23;        //-- Bits del prescaler
-parameter INI0 = 4'b1001; //-- Valor inicial para el registro 0
-parameter INI1 = 4'b0111; //-- Valor inicial para el registro 1
+reg inisig = 0;
+always @ (posedge(clk_pres))
+    inisig <= 1;
 
-//-- Reloj a la salida del presacaler
-wire clk_pres;
-
-//-- Salida de los regitros
-wire [3:0] dout0;
-wire [3:0] dout1;
-
-//-- Señal de inicializacion del reset
-reg rst = 0;
-
-//-- Inicializador
-always @(posedge(clk_pres))
-  rst <= 1;
-
-//-- Registro 0
-register #(.INI(INI0))
-  REG0 (
-    .clk(clk_pres),
-    .rst(rst),
-    .din(dout1),
-    .dout(dout0)
-  );
-
-//-- Registro 1
-register #(.INI(INI1))
-  REG1 (
-    .clk(clk_pres),
-    .rst(rst),
-    .din(dout0),
-    .dout(dout1)
-  );
-
-//-- Sacar la salida del registro 0 por la del componente
-assign data = dout0;
-
-//-- Prescaler
 prescaler #(.N(NP))
-  PRES (
-    .clk_in(clk),
-    .clk_out(clk_pres)
-  );
+    PRES1 (
+        .clk_in(clk),
+        .clk_out(clk_pres)
+    );
 
 endmodule
-
-
