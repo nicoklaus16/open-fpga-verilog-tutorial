1882ce94e654f18fc921f0e698c05f93e3fe0fb3
Nicolas Sanchez Flores <n.sanchez.f10@gmail.com>
2023-12-13 20:17:49 +0000

    working parametrisable register array using generate statement

--- tutorial/ICESTICK/T14-regreset/register.v
+++ tutorial/ICESTICK/T14-regreset/register.v
@@ -2,8 +2,8 @@ module register #(parameter WIDTH = 4, INI = 4'b0000)
 (
     input clk,
     input rst_bar,
-    input din [WIDTH-1:0],
-    output reg dout [WIDTH-1:0]
+    input [WIDTH-1:0] din,
+    output reg [WIDTH-1:0] dout
 );
 
 always @ (posedge(clk)) begin
--- tutorial/ICESTICK/T14-regreset/regreset.v
+++ tutorial/ICESTICK/T14-regreset/regreset.v
@@ -1,22 +1,9 @@
-module regreset #(parameter NP = 2)
+module regreset #(parameter NP = 2, NREG = 4)
 (
     input clk,
-    output data [3:0]
+    output [3:0] data
 );
-integer INI = 0;
-wire [1:0]outputs[3:0];
-
-genvar i;
-for (i = 0; i < 2; i=i+1) begin:reg_array 
-    INI = INI + 1;
-    register #(.WIDTH(4), .INI(INI))
-        dut (
-            .clk(clk_pres),
-            .rst_bar(inisig),
-            .din(outputs()),
-            .dout(output(i+1))
-        );
-end
+wire [3:0]outsignals[NREG-1:0];
 
 reg inisig = 0;
 always @ (posedge(clk_pres))
@@ -27,5 +14,28 @@ prescaler #(.N(NP))
         .clk_in(clk),
         .clk_out(clk_pres)
     );
+
+assign data = outsignals[0];
+
+genvar i;
+for (i = 0; i < NREG; i=i+1) begin:reg_array 
+    if (i == NREG-1) begin
+        register #(.WIDTH(4), .INI(i))
+            dutf (
+                .clk(clk_pres),
+                .rst_bar(inisig),
+                .din(outsignals[0]),
+                .dout(outsignals[i])
+            );
+    end else begin 
+        register #(.WIDTH(4), .INI(i))
+            dut1 (
+                .clk(clk_pres),
+                .rst_bar(inisig),
+                .din(outsignals[i+1]),
+                .dout(outsignals[i])
+            );
+    end
+end
 
 endmodule
